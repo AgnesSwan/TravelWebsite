a43623e7d4a180b922112de4f2d0e8c5cd752603
Agnieszka Szmidtke <agnes.swan7@gmail.com>
2020-03-09 22:58:07 +0100

    WithGallery

--- src/Photo/Photo.js
+++ src/Photo/Photo.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './Photo.css';
+import Mediolan from '../Image/Mediolan.jpg';
 const Photo = (props) => (
   <>
 
 
 
-    <div className="cloud2" >Photo</div>
+    <div className="cloud2" >
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div className="enter"></div>
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div class="zoomContainer">
+        <img width='300' src={Mediolan} alt="Mediolan2"></img>
+      </div>
+      <div className="enter"></div>
 
+    </div>
 
   </>
+
 );
 export default Photo;
