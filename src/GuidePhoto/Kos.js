186d60569e7078e417561d74699fe737626ff283
Agnieszka Szmidtke <agnes.swan7@gmail.com>
2020-03-14 14:26:09 +0100

    WersjaPrzewodnik1

--- src/GuidePhoto/Kos.js
+++ src/GuidePhoto/Kos.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import '../Guide/Guide.css';
+import './Kos.css';
+import Grecja from '../Image/Grecja.jpg';
 const Kos = props => (
-  <>
-    <div className="cloud3">
-      <h1>Grecja, Kos</h1>
-      <h2>W tydzień</h2>
+  <div className='Kos__card'>
+    <div className="Kos__header">
+      <h1>Grecja, Kos </h1>
+      <h2><i>W tydzień</i></h2>
     </div>
-  </>
+    <div className="box">
+      <div className="imgBox">
+        <img src={Grecja} alt="Greece" />
+      </div>
+      <div className="textCon">
+        <h3>Kos City</h3>
+        <p>asxdfcvghbklm</p>
+
+      </div>
+    </div>
+    <div className="enter"></div>
+    <div className="box" >
+      <div className="imgBox">
+        <img src={Grecja} alt="Greece" />
+      </div>
+      <div className="textCon">
+        <h3>Zia i Dikeos</h3>
+        <p>asxdfcvghbklm</p>
+      </div>
+    </div>
+  </div>
+
 );
 export default Kos; 
